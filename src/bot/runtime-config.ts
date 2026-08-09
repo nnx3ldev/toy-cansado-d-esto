73e05ec34a720748e9087a5a336dfdbf35621f70
nnx3l <nnx3l.dev@gmail.com>
2026-08-09 10:44:55 -0400

    Delete src directory

--- src/bot/runtime-config.ts
+++
@@ -1,8 +0,0 @@
-import { config as loadDotenv } from "dotenv";
-import { findAttachedAsset } from "./paths";
-
-const uploadedEnv = findAttachedAsset((file) => file.endsWith(".env"));
-if (uploadedEnv) {
-  loadDotenv({ path: uploadedEnv });
-}
-loadDotenv();
