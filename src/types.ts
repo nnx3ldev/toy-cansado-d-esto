da82ccd4f6bc7d90e79b2ca70d1e5d910398d971
nnx3l <nnx3l.dev@gmail.com>
2026-08-09 12:05:42 -0400

    Delete src directory

--- src/types.ts
+++
@@ -1,6 +0,0 @@
-import { ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
-
-export interface Command {
-  data: SlashCommandBuilder | Omit<SlashCommandBuilder, "addSubcommand" | "addSubcommandGroup">;
-  execute: (interaction: ChatInputCommandInteraction) => Promise<void> | void;
-}
